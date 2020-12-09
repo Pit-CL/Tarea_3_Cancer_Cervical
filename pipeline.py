94c235b5693f7e760c796fc6cd877b413a00c123
rfariaspoblete <rafaelfariaspoblete@gmail.com>
2020-12-09 18:07:04 -0300

    Beta1

--- pipeline.py
+++ pipeline.py
@@ -2,41 +2,50 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
 from sklearn import svm
-from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, \
-    GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.metrics import classification_report
 
 # Archivo que contiene los datos
 file = '/home/rafaelfp/Dropbox/Postgrados/MDS/ML/ML2/Tarea_3/data/sobar-72.csv'
 
-# Creo el df en pandas
+# Se crea el df en pandas
 df = pd.read_csv(file)
 
-# Reviso el balanceo de los datos de cáncer.
-# No es un df muy balanceado por lo que el accuracy por sí solo no será el
-# mejor indicador de la calidad del modelo.
-# Debo revisar precisión, matriz de confusión, recall y f-1.
-# Quizás DT o RF es el mejor para temas relacionados a clases no balanceadas ya
-# que trabaja aprendiendo por jerarquía por lo que ambas clases son alcanzadas.
+# Se revisa el data set
+head = df.head()
+print('Un resumen del DF\n', head)
+
+# Se revisa la info de la data para conocer las variables asociadas.
+info = df.info()
+print('Información de las variables\n', info)
+
+# Describe para ver rápidamente el comportamiento de las variables.
+describe = df.describe()
+print('Descripción estadística\n', describe)
+
+# Se revisa la distribución de los datos.
+sns.distplot(df)
+plt.show()
+
+# Se revisa el balanceo de los datos de cáncer.
 sns.countplot(x='ca_cervix', data=df)
 plt.title('Distribución de las clases objetivo')
 plt.show()
 
-# Reviso presencia de outliers.}
+# Se revisa presencia de outliers.
 plt.xticks(rotation=90)
 sns.boxplot(data=df)
 plt.xticks(rotation=90)
 plt.show()
 
-# Separando el df entre variables dependientes e independientes.
+# Separando el df
 x = df.iloc[:, :19]
 y = df.iloc[:, 19]
 
@@ -44,54 +53,37 @@ y = df.iloc[:, 19]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4,
                                                     random_state=42)
 
-# Creando los pipes utilizando las 4 variables cuya varianza explicada es
-# la más representativa. Esta representatividad está compuesta
-#  por con un 29%, con un 19%, con un 11% y con un 9%. Con esto se completa
-#  un 70% aproximado de representatividad de las variables.
-# TODO: completar el nombre de las variables más representativas.
-# TODO: Explicar porque se usa StandardScaler.
-pipe_rl = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
-                    ('clf', LogisticRegression(random_state=42))])
-
+# Creando los pipelines.
 pipe_svm = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
                      ('clf', svm.SVC(random_state=42))])
 
 pipe_RF = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
                     ('clf', RandomForestClassifier(random_state=42))])
 
-pipe_DT = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
-                    ('clf', DecisionTreeClassifier(random_state=42))])
-
-pipe_ADA = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
-                     ('clf', AdaBoostClassifier(random_state=42))])
-
-pipe_bagin = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
-                       ('clf', BaggingClassifier(random_state=42))])
-
 pipe_grad = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
                       ('clf', GradientBoostingClassifier(random_state=42))])
 
-pipe_MLCP = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
-                      ('clf', MLPClassifier(random_state=42, max_iter=3000))])
+pipe_BRF = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=4)),
+                     ('clf', BalancedRandomForestClassifier(random_state=42))])
 
 # Lista de pipelines
-pipelines = [pipe_rl, pipe_svm, pipe_RF, pipe_DT, pipe_ADA, pipe_bagin,
-             pipe_grad, pipe_MLCP]
+pipelines = [pipe_svm, pipe_RF, pipe_grad, pipe_BRF]
 
 # Fit a los pipelines
 for pipe in pipelines:
     pipe.fit(x_train, y_train)
 
+
 # Diccionario de los pipelines
-pipe_dict = {0: 'RL', 1: 'SVM', 2: 'RF', 3: 'DT', 4: 'ADA', 5: 'Bagin',
-             6: 'Gradient', 7: 'MLCP'}
+pipe_dict = {0: 'SVM', 1: 'RF', 2: 'GradienBoost', 3: 'BRF'}
+print('======================================================================')
 
 # Comparando accuracies.
 for idx, val in enumerate(pipelines):
     print('%s pipeline test accuracy: %.5f' % (
         pipe_dict[idx], val.score(x_test, y_test)))
 
-# Identificar el modelo con mejor accuracy en los datos de testeo.
+# Identificando el modelo con mejor accuracy en los datos de testeo.
 best_acc = 0.0
 best_clf = 0
 best_pipe = ''
@@ -100,20 +92,19 @@ for idx, val in enumerate(pipelines):
         best_acc = val.score(x_test, y_test)
         best_pipe = val
         best_clf = idx
-print('----------------------------------------------------------------------')
+print('======================================================================')
 print('El modelo clasificador con mejor accuracy es: %s' % pipe_dict[best_clf])
+print('======================================================================')
 
-# TODO: Al ejecutar el script nos damos cuenta que basados sólo en el accuracy
-# TODO: es poco probable determinar si RL es el mejor modelo ya que la data
-# TODO: está desbalanceada.
-# TODO: incluir matrices de confusión.
-# TODO: Para revisar la importancia de las variables debo hacer PCA
-# TODO: Ver si está balanceado o no.
-# TODo: Calcular el y_pred para imprimir las matrices de confusión.
-# TODO: Como random forest me entrega las variables importantes podría
-# TODO: seleccionar que variables entran
-# TODO: en 4 variables tengo el 70%
+# Imprimiendo las matrices de confusion.
+for idx, val in enumerate(pipelines):
+    print('Matriz de confusión para', pipe_dict[idx], 'es:\n', confusion_matrix
+          (y_test, val.predict(x_test)))
 
-# for idx, val in enumerate(pipelines):
-#     print('%s pipeline test accuracy: %.5f' % (
-#         pipe_dict[idx], confusion_matrix(y_test, predict_proba)))
+print('======================================================================')
+
+# Imprimiendo los reportes de clasificacion.
+for idx, val in enumerate(pipelines):
+    print('Reporte de clasificacion para', pipe_dict[idx], 'es:\n',
+          classification_report(y_test, val.predict(x_test)))
+
